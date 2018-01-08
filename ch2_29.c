9aae2201c0fb7c6b3bbfb1e0f7e1ed90cd120775
sowmiyaragupathi <34641629+sowmiyaragupathi@users.noreply.github.com>
2018-01-08 21:39:04 +0530

    Create ch2_29.c

---
+++ ch2_29.c
@@ -0,0 +1,20 @@
+//given time in minutes and print hours and minutes
+
+#include <stdio.h>
+int main(void)
+{
+	int a,b=0,h,m;;
+	scanf("%d",&a);
+	if(a<=0)
+	{
+		printf("%d",b);
+	}
+	else
+	{
+		h=a/60;
+		m=a%60;
+		printf("%d",h);
+		printf(" %d",m);
+	}
+	return 0;
+}
