db01659f1366cd2d43e0d67b13c73ce642697469
sowmiyaragupathi <34641629+sowmiyaragupathi@users.noreply.github.com>
2018-01-17 22:25:22 +0530

    Create ch2_47.c

---
+++ ch2_47.c
@@ -0,0 +1,46 @@
+//print the smallest and largest number in aaray
+
+#include<stdio.h>
+
+int main(void)
+
+{
+
+	int a[10],i,j,temp,n;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+
+	{
+
+		scanf("%d",&a[i]);
+
+	}
+
+	for(i=0;i<n;i++)
+
+	{
+
+		for(j=i+1;j<n;j++)
+
+		if(a[i] > a[j])
+
+		{
+
+			temp=a[i];
+
+			a[i]=a[j];
+
+			a[j]=temp;
+
+			
+
+		}
+
+	}
+
+	printf("%d",a[0]);
+	printf("\n%d",a[n-1]);
+
+	return 0;
+
+}
