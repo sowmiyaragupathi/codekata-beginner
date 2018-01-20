05c02f43ea9026206a7447ee35bd250c887732cc
sowmiyaragupathi <34641629+sowmiyaragupathi@users.noreply.github.com>
2018-01-20 19:14:49 +0530

    Create ch2_51.c

---
+++ ch2_51.c
@@ -0,0 +1,17 @@
+//print the number given by the user by spliting
+#include <stdio.h>
+int main(void) {
+	int n,temp,a,b,c,d;
+	scanf("%d",&n);
+	temp=n;
+	if(temp!=0)
+	{
+		a=temp/100;
+		b=temp%100;
+		c=b/10;
+		d=b%10;
+	}
+	printf("%d %d %d",a,c,d);
+	
+	return 0;
+}
