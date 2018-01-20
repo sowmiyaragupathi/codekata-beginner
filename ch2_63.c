1b3a0c260942b1ad50a649d38121212b5806008c
sowmiyaragupathi <34641629+sowmiyaragupathi@users.noreply.github.com>
2018-01-20 19:40:55 +0530

    Create ch2_63.c

---
+++ ch2_63.c
@@ -0,0 +1,33 @@
+// count the no of words in sentence
+
+#include <stdio.h>
+
+#include <string.h>
+
+int main(void)
+
+{
+
+    char s[200];
+
+    int count = 0, i,n,ans;
+
+    scanf("%[^\n]s", s);
+
+    n=strlen(s);
+
+    for (i = 0;s[i] != '\0';i++)
+
+    {
+
+        if (s[i] == ' ')
+
+            count++;    
+
+    }
+
+    printf("%d",count+1);
+
+   return 0;
+
+}
