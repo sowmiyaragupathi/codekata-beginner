4fef258e84b2817a0c90a905c6d24a15c5075ff2
sowmiyaragupathi <34641629+sowmiyaragupathi@users.noreply.github.com>
2018-01-20 19:25:35 +0530

    Create ch2_61.c

---
+++ ch2_61.c
@@ -0,0 +1,17 @@
+//print the  string first N characters 
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	char a[100];
+	int n,i,m;
+	scanf("%s",a);
+	scanf("%d",&n);
+	m=strlen(a);
+	for(i=0;i<n;i++)
+	{
+		printf("%c",a[i]);
+	}
+	
+	return 0;
+}
+
