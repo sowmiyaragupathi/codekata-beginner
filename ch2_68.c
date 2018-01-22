c6d4916d60eaec8e322e8c4db600babe752ed5cc
sowmiyaragupathi <34641629+sowmiyaragupathi@users.noreply.github.com>
2018-01-22 20:23:19 +0530

    Create ch2_68.c

---
+++ ch2_68.c
@@ -0,0 +1,16 @@
+//given number is  multiple of 7
+#include <stdio.h>
+int main(void) 
+{
+	int n;
+	scanf("%d",&n);
+	if(n%7==0)
+	{
+		printf("Yes");
+	}
+	else
+	{
+		printf("No");
+	}
+	return 0;
+}
