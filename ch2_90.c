1587768e6422e3e79bb32beedb0a722727cbcf1c
sowmiyaragupathi <34641629+sowmiyaragupathi@users.noreply.github.com>
2018-02-06 23:05:28 +0530

    Create ch2_90.c

---
+++ ch2_90.c
@@ -0,0 +1,19 @@
+//print only the numbers in a given alphanumeric stirng
+
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+	char a[10];
+	int n,i;
+	scanf("%s",a);
+	n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+		if(a[i]=='0'||a[i]=='1'||a[i]=='2'||a[i]=='3'||a[i]=='4'||a[i]=='5'||a[i]=='6'||a[i]=='7'||a[i]=='8'||a[i]=='9')
+		printf("%c",a[i]);
+	}
+
+	
+	return 0;
+}
