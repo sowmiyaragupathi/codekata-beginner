dc4e73c09ed5ac43ab1dfad3cbe941f6441bb4e9
sowmiyaragupathi <34641629+sowmiyaragupathi@users.noreply.github.com>
2018-03-08 21:43:52 +0530

    Create ch2_109.c

---
+++ ch2_109.c
@@ -0,0 +1,26 @@
+//find the minimum number
+#include <stdio.h>
+int main(void)
+{
+ int i, j, a, n, number[30];
+ scanf("%d", &n);
+ for (i = 0; i < n; ++i)
+ scanf("%d", &number[i]);
+ for (i = 0; i < n; ++i) 
+ {
+ for (j = i + 1; j < n; ++j)
+  {
+     if (number[i] > number[j]) 
+                {
+ 
+                    a =  number[i];
+                    number[i] = number[j];
+                    number[j] = a;
+ 
+                }
+ 
+   }
+}
+            printf("%d", number[0]);
+ 
+    }
