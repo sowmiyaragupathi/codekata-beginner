ed3183b0854497aa3af988d63c407faf9813272c
sowmiyaragupathi <34641629+sowmiyaragupathi@users.noreply.github.com>
2018-03-08 22:21:34 +0530

    Create ch2_112.c

---
+++ ch2_112.c
@@ -0,0 +1,27 @@
+//check whether the given no is in the array
+#include <stdio.h>
+int main(void)
+{
+int n,k,a[100],i,temp=0;
+scanf("%d %d",&n,&k);
+for(i=0;i<n;i++)
+{
+	scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+	if(a[i]==k)
+	{
+	temp++;
+	}
+}
+if(temp>0)
+{
+	printf("yes");
+}
+else
+{
+	printf("no");
+}
+return 0;
+}
