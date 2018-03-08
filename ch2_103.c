a0b5bbf4a70520ecfe6c6b560fdf85da949fffa7
sowmiyaragupathi <34641629+sowmiyaragupathi@users.noreply.github.com>
2018-03-08 22:08:42 +0530

    Create ch2_103.c

---
+++ ch2_103.c
@@ -0,0 +1,34 @@
+//captialize the first letter in a word
+#include <stdio.h>
+#include<string.h>
+int main(void)
+{
+ char a[100];
+
+    int n,i;
+
+    scanf("%[^\t\n]s",a);
+
+    n=strlen(a);
+
+    a[0]=a[0]-32;
+
+    for(i=0;i<n;i++)
+
+    {
+
+      if(a[i]==' ')
+
+      {
+
+          a[i+1]=a[i+1]-32;
+
+      }
+
+    }
+
+    printf("%s",a);
+
+	
+ return 0;
+}
